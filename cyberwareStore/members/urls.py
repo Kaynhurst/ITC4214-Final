36aff8281cb8805875ca7f9d8af3a37a45f5af4f
Kaynhurst <m.strintzis@acg.edu>
2024-07-14 23:05:23 +0300

    Login Page added

--- cyberwareStore/members/urls.py
+++ cyberwareStore/members/urls.py
@@ -7,5 +7,6 @@ app_name = 'members'
 
 urlpatterns = [
     path('login/' , views.login , name ="login"),
-    path('register/',views.register, name ="register")
+    path('register/',views.register, name ="register"),
+    path('profile/<str:name>/',views.profile,name="profile")
 ]
