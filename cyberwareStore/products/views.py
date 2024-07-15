cb89fed627bd04c4d425a65401fedb5b91a25630
Kaynhurst <m.strintzis@acg.edu>
2024-07-16 00:38:24 +0300

    Binary File changes

--- cyberwareStore/products/views.py
+++ cyberwareStore/products/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'products/index.html',{})
+    return render(request,'products/index.html')
 
 def cart(request):
-    return render(request,'products/cart.html',{})
+    return render(request,'products/cart.html')
