7e1bded4394c9718899520be965007cc2b79fe5a
Kaynhurst <m.strintzis@acg.edu>
2024-07-18 12:47:07 +0300

    Database intergration

--- cyberwareStore/products/models.py
+++ cyberwareStore/products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Category (models.Model):
+    categoryName = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return (f"Category ID : {self.id} + Category Name : {self.categoryName}")
+    
+
+class Grade (models.Model):
+    gradeName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (f"Grade ID : {self.id} + Grade Name : {self.gradeName}")
+    
+
+class Product(models.Model):
+    productName = models.CharField(max_length=50)
+    productPrice = models.FloatField()
+    productGrade = models.ForeignKey(Grade,on_delete=models.CASCADE)
+    productCategory = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"Product ID : {self.id} + Product Name : {self.productName} + Price : {self.productPrice} + Grade : {self.productGrade.gradeName} + Category : {self.productCategory.categoryName}")
+    
+
+class CheckoutCart (models.Model):
+    item = models.ForeignKey(Product,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"Product Name : {self.item.productName} + Product Price : {self.item.productPrice}")
--- cyberwareStore/products/views.py
+++ cyberwareStore/products/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index(request):
-    return render(request,'products/index.html')
+    productsList = Product.objects.all()
+    gradeList = Grade.objects.all()
+    categoryList = Category.objects.all()
+    return render(request,'products/index.html',{'productsList':productsList , 'gradeList':gradeList, 'categoryList' : categoryList})
 
 def cart(request):
-    return render(request,'products/cart.html')
+    
+
+    return render(request,'products/cart.html',)
